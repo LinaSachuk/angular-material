4b86877d9e1c1caf584145bc00a03546f8ab33e9
Lina Sachuk <sachuklina@gmail.com>
2021-02-16 00:25:48 -0700

    add routerLink

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MessageListComponent  } from './messages/message-list/message-list.component';
+import { OrderListComponent  } from './orders/order-list/order-list.component';
+import { CustomerListComponent  } from './customers/customer-list/customer-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'customers',
+    component: CustomerListComponent,
+  },
+  {
+    path:'orders',
+    component: OrderListComponent,
+  },
+  {
+    path:'messages',
+    component: MessageListComponent,
+  },
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,23 +6,27 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
-
+// Routing
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
-
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+
+    // Material
     BrowserAnimationsModule,
-    MatIconModule,
     MatButtonModule,
-    MatSidenavModule
+    MatIconModule,
+    MatSidenavModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
