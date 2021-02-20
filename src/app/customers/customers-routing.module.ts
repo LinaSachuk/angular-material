c4c659449a2489d6e74f8a361136bc8c85408d24
Lina Sachuk <sachuklina@gmail.com>
2021-02-20 14:54:10 -0700

    add avatar

--- src/app/customers/customers-routing.module.ts
+++ src/app/customers/customers-routing.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CustomerListComponent } from './customer-list/customer-list.component';
-
+import { CustomerCardComponent } from './customer-card/customer-card.component';
 
 const routes: Routes = [
   {
-    path: '',
+    path:'',
     component: CustomerListComponent
+  },
+  {
+    path:'info',
+    component: CustomerCardComponent
   }
-
-]
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
